059b2f760811638262e9776448559e14e0e7539a
Sudocker <sudhakar82@gmail.com>
2023-09-22 19:27:32 +0530

    [Part 2] Create simple LLM powered API

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+from langchain.llms import OpenAI
+from env import OPENAI_API_KEY
 
 app = Flask(__name__)
 
@@ -6,3 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Hello <b>AI</b> world"
+
+@app.post("/chat")
+def chat():
+  question = request.json["question"]
+  llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+  return llm.predict(question)
